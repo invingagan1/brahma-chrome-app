d392b6df0355ca85997f6a000dc311db56c00fd4
Gagan Deep Singh <Gagandeep.singh@barco.com>
2019-02-28 17:10:59 +0530

    removed files

--- background/window.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(() => {
-    chrome.app.window.create('../app/main.html', {
-        'outerBounds': {
-            width: 500,
-            height: 500
-        }
-    })
-});
